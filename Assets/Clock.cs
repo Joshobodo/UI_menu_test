a63699bfa8f1d2150a6074ce64b5acad7cafe858
Joshobodo <joshobodo@hotmail.com>
2024-03-21 15:27:10 +1100

    Initial commit

---
+++ Assets/Clock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System; 
+
+public class Clock : MonoBehaviour
+{
+    public TMP_Text clockText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
